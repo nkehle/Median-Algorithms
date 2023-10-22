0ad899e2ebb7bdb60b3165a4bfa7e872e6c2f7bf
Noa Kehle <nkehle@calpoly.edu>
2023-10-21 21:55:48 -0700

    Connecting to repo

---
+++ QuickSortMedian.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+''' Purpose: Sort the arr and pick middle element
+    Parameter1 -> Array
+    Return     -> Median   '''
+def find_median(arr):
+    merge_sort(arr)
+    return arr[len(arr) // 2]       # middle (median)
+
+''' Purpose: Sort the arr 
+    Parameter1 -> Array
+    Return     -> none   '''
+def merge_sort(arr):
+    # base case -> when there is one element
+    if len(arr) <= 1:
+        return arr[0]
+
+    # find the middle index to split array into two
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    # call the sort on the two halves
+    merge_sort(left)
+    merge_sort(right)
+
+    # merge the two
+    merge(arr, left, right)
+
+
+''' Purpose: to combine the two arrays
+    Parameter1 -> Array
+    Return     -> Median   '''
+def merge(arr, left, right):
+    # for indexing the left right and res
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    # clear the rest of both
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while k < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+"""
+arr = [5,4,3,2,1,]
+
+print("Array:", arr, "\nMedian --> ", find_median(arr))
+"""
---
+++ RandomMedian.py
@@ -0,0 +1,78 @@
+import numpy as np
+import QuickSortMedian
+
+''' Purpose: Sort the arr and pick middle element
+    Parameter1 -> Array
+    Return     -> Median   '''
+def find_median(arr, k):
+    if len(arr) <= 5:
+        QuickSortMedian.merge_sort(arr)
+        return arr[k]
+
+    pivot = arr[k]     # random element in the middle
+
+    left = list()
+    right = list()
+
+    for num in arr:
+        if num < pivot:
+            left.append(num)
+        else:
+            right.append(num)
+
+    pivot_index = len(left)+1
+
+    if pivot_index == k:
+        return arr[pivot_index]
+    elif pivot_index < k:
+        find_median(right, k - pivot_index - 1)
+    else:
+        find_median(left, k)
+
+arr = [3,2,1,4,5]
+print("Median: ", find_median(arr, 2))
+'''
+def into_groups(arr, n):
+    groups = list()
+    count = 0
+    for element in arr:
+'''
+
+'''PsedoCode
+
+groups = into_groups(arr, 5)
+    medians = list()
+    for group in groups:
+        medians.append(small_median(group))
+    pivot = find_median(medians, len(medians)/2)
+
+function MedianOfMedians(arr, k)
+    if length(arr) <= 5
+        Sort(arr)
+        return arr[k]
+
+    groups = DivideIntoGroups(arr, 5)
+    medians = [FindMedian(group) for group in groups]
+    pivot = MedianOfMedians(medians, len(medians) / 2)
+
+    smaller = [element for element in arr if element < pivot]
+    larger = [element for element in arr if element > pivot]
+    equal = [element for element in arr if element == pivot]
+
+    if k < len(smaller)
+        return MedianOfMedians(smaller, k)
+    else if k < len(smaller) + len(equal)
+        return pivot
+    else
+        return MedianOfMedians(larger, k - len(smaller) - len(equal)
+
+function FindMedian(arr)
+    Sort(arr)
+    return arr[len(arr) / 2]
+
+function DivideIntoGroups(arr, groupSize)
+    groups = []
+    for i from 0 to len(arr) step groupSize
+        groups.append(arr[i:i + groupSize])
+    return groups
+'''
